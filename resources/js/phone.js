37ef7f4c39ed94654600e942fa9a74186dc2c1eb
brito101 <rodrigobrito101@hotmail.com>
2024-08-17 18:25:07 -0300

    fix: rent price in admin dash

--- resources/js/phone.js
+++ resources/js/phone.js
@@ -1,4 +1,4 @@
 $(document).ready(function () {
-    $("#telephone").inputmask("(99)-9999-9999");
-    $("#cell").inputmask("(99)-99999-9999");
+    $("#telephone").inputmask("(99) 9999-9999");
+    $("#cell").inputmask("(99) 99999-9999");
 });
